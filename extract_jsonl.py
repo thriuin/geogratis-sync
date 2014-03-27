1f8e0a3fbe50a05f204d116a1171d12cd885cae2
Ross Thompson <ross.thompson@statcan.gc.ca>
2014-03-27 16:42:48 -0400

    Add simple file to extract CKAN JSONL from the database

--- extract_jsonl.py
+++ extract_jsonl.py
@@ -1,10 +1,22 @@
-__author__ = 'thomros'
+__author__ = 'Statistics Canada'
+__copyright__ = 'Crown Copyright'
+__license__ = 'MIT'
 
+import argparse
 from db_schema import connect_to_database, find_all_geogratis_records
+
+argparser = argparse.ArgumentParser(description='Scan Geogratis and save record to a database')
+argparser.add_argument('-o', '--outfile',
+                       action='store',
+                       default='geogratis_update.jsonl',
+                       dest='outfile',
+                       help='Write extracted CKAN JSONL to this file')
+args = argparser.parse_args()
 
 session = connect_to_database()
 known_records = find_all_geogratis_records(session)
-jfile = open('geo.jsonl', mode='w')
+jfile = open(args.outfile, mode='w')
 for r in known_records:
-    print >> jfile, r.ckan_json
+    if r.od_status == 'Needs Update' or r.od_status == 'New Record' and r.ckan_json is not None:
+        print >> jfile, r.ckan_json
 jfile.close()
