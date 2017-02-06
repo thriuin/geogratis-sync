05e36416982029808343abdd04625d1ac86e606c
thriuin <ross.thompson@canada.ca>
2017-02-06 15:55:51 -0500

    Add step to convert metadata from 2.3 to 2.5.

--- daily_scan.sh
+++ daily_scan.sh
@@ -7,8 +7,18 @@ if [ "$#" -ne 2 ]; then
   exit 1
 fi
 
+PYTHON_VERSION=`python -c "import sys;t=int('{0}{1}'.format(sys.version_info[0],sys.version_info[1]));sys.exit(t)"`
+
+if [ $PYTHON_VERSION -eq 27 ]
+then
+  echo `which python`
+  echo "Requires Python 2.7"
+  exit
+fi
+
 LOCKFILE=./.daily_scan.lock
 DUMPFILE=./geo-$(date +"%Y_%m_%d").jsonl
+XFORMFILE=./geo-$(date +"%Y_%m_%d")-xform.jsonl
 
 if [ -e ${LOCKFILE} ] && kill -0 `cat ${LOCKFILE}`; then
     echo "already running"
@@ -34,8 +44,15 @@ python converter.py -m
 echo "Step 3: Extracting latest data for CKAN"
 python dump_geogratis.py -m -f ${DUMPFILE}
 
-echo "Step 4: Load the scanned data into the local CKAN"
-ckanapi load datasets -I ${DUMPFILE} -c $2 -l ckanapi-load.log
+echo "Step 4: Transform the 2.3 json to version 2.5"
+if [ -f  ${DUMPFILE} ]; then 
+  cat  ${DUMPFILE} | paster canada metadata-xform --portal -c /var/www/html/open_gov/staging-portal/ckan/production.ini > $XFORMFILE
+
+  echo "Step 5: Load the scanned data into the local CKAN"
+  ckanapi load datasets -I ${XFORMFILE} -c $2 -l ckanapi-load.log
+else
+  echo "No GeoGratis records found to be loaded"
+fi
 
 # Done
 
